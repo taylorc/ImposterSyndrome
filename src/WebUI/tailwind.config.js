6bc1d855ef0b1454f4bdd6665d573ad2f3597904
Craig Taylor <taylorc5150@gmail.com>
2025-08-28 15:17:21 +1000

    added tailwind artefacts

---
+++ src/WebUI/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{vue,js,ts,jsx,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
