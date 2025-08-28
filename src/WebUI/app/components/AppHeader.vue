dc2e9959580eeb6491042a66fbc036026d121389
Craig Taylor <taylorc5150@gmail.com>
2025-08-28 13:10:11 +1000

    added nux to aspire orchestration

--- src/WebUI/app/components/AppHeader.vue
+++ src/WebUI/app/components/AppHeader.vue
@@ -2,8 +2,8 @@
  <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <a href="#" class="flex items-center">
-            <img src="https://www.svgrepo.com/show/499962/music.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Landwind</span>
+            <img src="https://www.svgrepo.com/show/499962/music.svg" class="h-6 mr-3 sm:h-9" alt="Imposter Syndrome Logo">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Imposter Syndrome</span>
         </a>
         <div class="flex items-center lg:order-2">
             <div class="hidden mt-2 mr-4 sm:inline-block">
